89625c711fc42d49fa913208cce6aab986f8ec7e
Cedrik Hoffmann <cedrik.hoffmann@stud.hs-flensburg.de>
2021-11-25 17:22:07 +0100

    init compose projekt

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    // __KOTLIN_COMPOSE_VERSION__
+    kotlin("jvm") version "1.5.31"
+    // __LATEST_COMPOSE_RELEASE_VERSION__
+    id("org.jetbrains.compose") version (System.getenv("COMPOSE_TEMPLATE_COMPOSE_VERSION") ?: "1.0.0-beta5")
+}
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+dependencies {
+    // Note, if you develop a library, you should use compose.desktop.common.
+    // compose.desktop.currentOs should be used in launcher-sourceSet
+    // (in a separate module for demo project and in testMain).
+    // With compose.desktop.common you will also lose @Preview functionality
+    implementation(compose.desktop.currentOs)
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            appResourcesRootDir.set(project.layout.projectDirectory.dir("xxx"))
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "KotlinJvmComposeDesktopApplication"
+            packageVersion = "1.0.0"
+        }
+    }
+}
